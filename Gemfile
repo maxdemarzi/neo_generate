7253db899ce5f18c04ffade48709c5f9d78ff011
Max De Marzi <maxdemarzi@hotmail.com>
2012-06-29 17:39:39 -0500

    adding forgery and connection to relationship creation

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'neography'
-gem 'json'
+gem 'forgery'
--- neo_generate.rb
+++ neo_generate.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'neography'
+require 'forgery'
 require 'uri'
 
   # Generate sample data and load into graph
@@ -20,19 +21,36 @@ require 'uri'
   #  Item             -[:tagged]->     Tags
   #
   def create_graph
-    # Stage Node Property Values
-    node_values = [lambda { generate_text }, lambda { generate_text }]
-    
+    create_node_properties
+    create_nodes
+    create_relationship_properties
+    create_relationships
+  end  
+
+  def create_node_properties
     # Stage Nodes
     node_properties = ["property1", "property2"]
-    generate_node_properties(node_properties)
+    generate_node_properties(node_properties)  
+  end
+
+  def create_relationship_properties
+    # Stage Relationships
+    rel_properties = ["property1", "property2"]
+    generate_rel_properties(rel_properties)
+  end
+
+  def create_nodes
+    # Define Node Property Values
+    node_values    = [lambda { generate_text              }, lambda { generate_text              }]
+    user_values    = [lambda { Forgery::Name.full_name    }, lambda { Forgery::Personal.language }]
+    company_values = [lambda { Forgery::Name.company_name }, lambda { Forgery::Name.industry     }]
     
-    nodes = {"user"     => { "start" => 1,
+   @nodes = {"user"     => { "start" => 1,
                              "end"   => 21000, 
-                             "props" => node_values},
+                             "props" => user_values},
              "company"  => { "start" => 21001,
                              "end"   => 25000, 
-                             "props" => node_values},
+                             "props" => company_values},
              "activity" => { "start" => 25001,
                              "end"   => 1225000, 
                              "props" => node_values},
@@ -48,45 +66,48 @@ require 'uri'
     }
     
     # Write nodes to file
-    nodes.each{ |node| generate_nodes(node[0], node[1])}
+    @nodes.each{ |node| generate_nodes(node[0], node[1])}
+  end
 
-    # Stage Relationsihp Property Values
+  def create_relationships
+    # Define Relationsihp Property Values
     rel_values = [lambda { generate_text }, lambda { generate_text }]
 
-    # Stage Relationships
-    rel_properties = ["property1", "property2"]
-    generate_rel_properties(rel_properties)
-
-    rels = {"user_to_company"  => { "from"  => nodes["user"],
-                                    "to"     => nodes["company"],
+    rels = {"user_to_company"  => { "from"  => @nodes["user"],
+                                    "to"     => @nodes["company"],
                                     "number" => 21000,
                                     "type"   => "belongs_to",
-                                    "props"  => rel_values},
-            "user_to_activity" => { "from"  => nodes["user"],
-                                    "to"    => nodes["activity"],
+                                    "props"  => rel_values,
+                                    "connection" => :sequential },
+            "user_to_activity" => { "from"  => @nodes["user"],
+                                    "to"    => @nodes["activity"],
                                     "number" => 1200000,
                                     "type"   => "performs",
-                                    "props"  => rel_values},
-            "activity_to_item" => { "from"  => nodes["activity"],
-                                    "to"    => nodes["item"],
+                                    "props"  => rel_values,
+                                    "connection" => :random },
+            "activity_to_item" => { "from"  => @nodes["activity"],
+                                    "to"    => @nodes["item"],
                                     "number" => 3000000,
                                     "type"   => "belongs",
-                                    "props"  => rel_values},
-            "item_to_entity"   => { "from"  => nodes["item"],
-                                    "to"    => nodes["entity"],
+                                    "props"  => rel_values,
+                                    "connection" => :random },
+            "item_to_entity"   => { "from"  => @nodes["item"],
+                                    "to"    => @nodes["entity"],
                                     "number" => 6000000,
                                     "type"   => "references",
-                                    "props"  => rel_values},
-            "item_to_tag"      => { "from"  => nodes["item"],
-                                    "to"    => nodes["tag"],
+                                    "props"  => rel_values,
+                                    "connection" => :random },
+            "item_to_tag"      => { "from"  => @nodes["item"],
+                                    "to"    => @nodes["tag"],
                                     "number" => 250000,
                                     "type"   => "tagged",
-                                    "props"  => rel_values}                                   
+                                    "props"  => rel_values,
+                                    "connection" => :random }                                   
     }
   
     # Write relationships to file
-    rels.each{ |rel| generate_rels(rel[1])}
-  end  
+    rels.each{ |rel| generate_rels(rel[1])}  
+  end
 
   #  Recreate nodes.csv and set the node properties 
   #  
@@ -118,8 +139,18 @@ require 'uri'
   def generate_rels(hash)
     puts "Generating #{hash["number"]} #{hash["type"]} relationships..."
     File.open("rels.csv", "a") do |file|
-      hash["number"].times do |t|
-        file.puts "#{rand(hash["from"]["start"]..hash["from"]["end"])}\t#{rand(hash["to"]["start"]..hash["to"]["end"])}\t#{hash["type"]}\t#{hash["props"].collect{|l| l.call}.join("\t")}" 
+    
+      case hash["connection"]
+        when :random      
+          hash["number"].times do |t|
+            file.puts "#{rand(hash["from"]["start"]..hash["from"]["end"])}\t#{rand(hash["to"]["start"]..hash["to"]["end"])}\t#{hash["type"]}\t#{hash["props"].collect{|l| l.call}.join("\t")}" 
+          end
+        when :sequential
+          from_size = hash["from"]["end"] - hash["from"]["start"]
+          to_size = hash["to"]["end"] - hash["to"]["start"]
+          hash["number"].times do |t|
+            file.puts "#{hash["from"]["start"] + (t % from_size)}\t#{hash["to"]["start"]  + (t % to_size)}\t#{hash["type"]}\t#{hash["props"].collect{|l| l.call}.join("\t")}" 
+          end
       end
     end
   end
@@ -144,3 +175,15 @@ require 'uri'
     puts "Copy and Paste the following:"
     puts "java -server -Xmx4G -jar ../batch-import/target/batch-import-jar-with-dependencies.jar neo4j/data/graph.db nodes.csv rels.csv"    
   end 
+  
+  def index_graph
+    neo = Neography::Rest.new
+    begin
+      if neo.execute_script("g.indices;").empty?  
+        neo.execute_script("g.createAutomaticIndex('vertices', Vertex.class, null);") 
+        neo.execute_script("AutomaticIndexHelper.reIndexElements(g, g.idx('vertices'), g.V);") if neo.execute_script("g.V.count();").to_i > 0
+      end
+    rescue Timeout::Error 
+      puts "Indexing the graph is going to take some time, watch it on #{ENV['NEO4J_URL'] || "http://localhost:7474"}"
+    end
+  end
